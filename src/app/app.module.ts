572eb17f52b36c6525feead9d37f15c66d1031ad
Velid Duranovic <velid_work@outlook.com>
2019-10-03 09:33:57 +0200

    Added header and routes to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,25 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product-list/product/product.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ImageDescriptionComponent } from './product-list/product/image-description/image-description.component';
+import { HeaderComponent } from './header/header.component';
+import { DummyComponent } from './dummy/dummy.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ProductListComponent,
-    ImageDescriptionComponent
+    ImageDescriptionComponent,
+    HeaderComponent,
+    DummyComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      {path: 'dummy', component: DummyComponent},
+      {path: 'product-list', component: ProductListComponent},
+      {path: 'welcome', redirectTo: '/welcome'}
+    ]),
     BrowserModule
   ],
   providers: [],
